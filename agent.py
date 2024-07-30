2927bf80ec0428149aea3a4c82807a1467d15cee
JmuneraCLQ <jmunera@clq.com.co>
2024-07-30 16:56:45 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ import re
 import matplotlib.pyplot as plt
 from langchain_experimental.agents import create_csv_agent
 from langchain.chat_models import ChatOpenAI
-from langchain_experimental.agents.agent_types import AgentType
+from langchain.agents.agent_types import AgentType
 
 
 # Setting up the api key
@@ -75,42 +75,3 @@ def extract_code_from_response(response):
         return match.group(1).strip()
     return None
 
-def csv_analyzer_app():
-    """Main Streamlit application for CSV analysis."""
-
-    st.title('CSV Assistant')
-    st.write('Please upload your CSV file and enter your query below:')
-    
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    
-    if uploaded_file is not None:
-        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
-        st.write(file_details)
-        
-        # Save the uploaded file to disk
-        file_path = os.path.join("/tmp", uploaded_file.name)
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        
-        df = pd.read_csv(file_path)
-        st.dataframe(df)
-        
-        user_input = st.text_input("Your query")
-        if st.button('Run'):
-            response = csv_agent_func(file_path, user_input)
-            
-            # Extracting code from the response
-            code_to_execute = extract_code_from_response(response)
-            
-            if code_to_execute:
-                try:
-                    # Making df available for execution in the context
-                    exec(code_to_execute, globals(), {"df": df, "plt": plt})
-                    fig = plt.gcf()  # Get current figure
-                    st.pyplot(fig)  # Display using Streamlit
-                except Exception as e:
-                    st.write(f"Error executing code: {e}")
-            else:
-                st.write(response)
-
-    st.divider()
