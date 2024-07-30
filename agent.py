dc528e9b16e35017f5785c127ebd626dd2822fe2
JmuneraCLQ <jmunera@clq.com.co>
2024-07-30 16:53:44 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ import os
 import re
 import matplotlib.pyplot as plt
 from langchain_experimental.agents import create_csv_agent
-from langchain.chat_models import ChatOpenAI
+from langchain_experimental.chat_models import ChatOpenAI
 from langchain.agents.agent_types import AgentType
 
 
